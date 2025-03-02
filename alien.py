f8b8b09c5e2a53de98909441e1367ff05047f207
weinaoa <weinaoa233@gamil.com>
2025-03-02 13:11:55 +0800

    third commit

--- alien.py
+++ alien.py
@@ -11,7 +11,7 @@ class Alien(Sprite):
         self.settings = ai_game.settings
 
         # 加载外星人图像并设置其rect属性
-        self.image = pygame.image.load('images/alien.bmp')
+        self.image = pygame.image.load('images/beiliya.png')
         self.rect = self.image.get_rect()
 
         # 每个外星人最初都在屏幕左上角附近
--- ship.py
+++ ship.py
@@ -10,7 +10,7 @@ class Ship(object):
         self.screen_rect = ai_game.screen.get_rect()
 
         # 加载飞船图像并获取其外接矩形。
-        self.image = pygame.image.load("images\\ship.bmp")
+        self.image = pygame.image.load("images\\nailong.png")
         self.rect = self.image.get_rect()
 
         # 对于每艘新飞船，都将其放在屏幕底部的中央。
